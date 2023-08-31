3835a2ea108757786d43a7e42bf9a5feba891aa8
Nguyễn Đăng Nhân <2051063581@e.tlu.edu.vn>
2023-09-01 02:14:53 +0700

    fix bug code
    footer

--- LaravelTest2/example-app/resources/views/layouts/clients/footer.blade.php
+++ LaravelTest2/example-app/resources/views/layouts/clients/footer.blade.php
@@ -3,10 +3,11 @@
         <div class="row px-xl-5 pt-5">
             <div class="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
                 <h5 class="text-secondary text-uppercase mb-4">Get In Touch</h5>
-                <p class="mb-4">No dolore ipsum accusam no lorem. Invidunt sed clita kasd clita et et dolor sed dolor. Rebum tempor no vero est magna amet no</p>
-                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>info@example.com</p>
-                <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+012 345 67890</p>
+                <p class="mb-4">
+As an information technology student with a passion for web programming, I would like to find a position where I can learn and work to develop skills and gain experience to become a WebDeveloper.</p>
+                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>Ha Noi, Viet Nam</p>
+                <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>nguyendangnhan572002@gmail.com</p>
+                <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+84 113 114 115</p>
             </div>
             <div class="col-lg-8 col-md-12">
                 <div class="row">
@@ -57,9 +58,8 @@
         <div class="row border-top mx-xl-5 py-4" style="border-color: rgba(256, 256, 256, .1) !important;">
             <div class="col-md-6 px-xl-0">
                 <p class="mb-md-0 text-center text-md-left text-secondary">
-                    &copy; <a class="text-primary" href="#">Domain</a>. All Rights Reserved. Designed
-                    by
-                    <a class="text-primary" href="https://htmlcodex.com">HTML Codex</a>
+                #E-Commerce Website Developed by
+                    <a class="text-primary" href="https://github.com/people0507">Nguyen Dang Nhan</a>
                 </p>
             </div>
             <div class="col-md-6 px-xl-0 text-center text-md-right">
